5142ff8a87a2aeedb27426d6450be46672baa852
For-b <ffranzen@greatriverschool.org>
2019-02-19 15:49:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name="Making a bot"))
 
 @client.event
-async def on_message_delete(message):
+async def on_message(message):
     author = message.author
     message = message.content
     print('{}: {}'.format(author, message))
+
+@client.event
+async def on_message_delete(message):
+    author = message.author
+    message = message.content
+    print('{}, deleted: {}'.format(author, message))
 
 @client.event
 async def on_message(message):
@@ -19,5 +25,12 @@ async def on_message(message):
         return
     if message.content == "+help":
         await client.send_message(message.channel, "piss off i aint finished!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "+yeet":
+        await client.send_message(message.channel, "yote")
 
 client.run('NTQ3MTY3MDkzOTkzMzczNzA2.D0y0ng.AAM18D03m1Uw7BX4f_I1sSCygRQ')
